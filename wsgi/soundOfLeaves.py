368f0607486e8d7e7f870a2fdc58c058a4eacd89
Raymond Tang <Raytray25@gmail.com>
2013-01-19 17:38:18 -0500

    Cleaned up code and removed some unnecessary files

--- wsgi/soundOfLeaves.py
+++ wsgi/soundOfLeaves.py
@@ -1,17 +1,17 @@
-import pprint
-import urllib
-import json
-import sys
-
+import urllib, json, sys
+#import json
+#import sys #Why is this here? Do we need it?
 from flask import Flask, render_template, request
-from pyechonest import config
-from pyechonest import song
+from pyechonest import config, song
+#from pyechonest import song
+
 config.ECHO_NEST_API_KEY="IY52OEPU4LRFUIY8R"
+ALCHEMY_API_KEY="6b961c784967a94fe1829d3d065016f87bf38153"
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "index"
+    return render_template('ray.html')
 
 @app.route("/text", methods=['POST', 'GET'])
 def text():
@@ -20,11 +20,7 @@ def text():
         return text_analyzed(request.form['url'])
     return render_template('text.html', error=error)
 
-@app.route("/ray")
-def ray():
-    return render_template('ray.html')
-
-@app.route('/connie', methods=['POST'])
+@app.route('/playlist', methods=['POST'])
 def connie(URL=None):
     mood = texts(request.form['URL'])
     songlist = song.search(mood, buckets=['tracks','id:spotify-WW'], limit=True, results=5)
@@ -34,18 +30,14 @@ def connie(URL=None):
             foreign_ids.append(t['foreign_id'][17:])
         comma = ","
         foreign_id_string = comma.join(foreign_ids)
-    print foreign_id_string
-    return render_template("spotify.html", tracks=foreign_id_string)
+    return render_template("playlist.html", tracks=foreign_id_string)
 
 def texts(name):
-    analyzeURL = "http://access.alchemyapi.com/calls/url/URLGetTextSentiment?apikey=6b961c784967a94fe1829d3d065016f87bf38153&outputMode=json&url=" + name
+    global ALCHEMY_API_KEY
+    analyzeURL = "http://access.alchemyapi.com/calls/url/URLGetTextSentiment?apikey=" + ALCHEMY_API_KEY + "&outputMode=json&url=" + name
     jsonResponse=json.loads(urllib.urlopen(analyzeURL).read())
-    #do a check for status ok.
-    #then go into the dictionary for the docSentiment and pull out the type.
-    pprint.pprint(jsonResponse)
     docSentiment = jsonResponse[u'docSentiment']
     mood = docSentiment[u'type']
-    print mood
     return mood
 
 if __name__ == '__main__':
