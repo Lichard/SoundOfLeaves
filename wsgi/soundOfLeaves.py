9d6c978b5e95ca49c7fc29967eb40cba42467747
Richard <ryli721@gmail.com>
2013-01-19 13:39:04 -0500

    ready to merge

--- wsgi/soundOfLeaves.py
+++ wsgi/soundOfLeaves.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
@@ -12,6 +12,15 @@ def ray():
 @app.route('/hello/<name>')
 def hello(name=None):
     return render_template('hello.html', name=name)
+@app.route('/connie', methods=['POST','GET'])
+def connie():
+   return 'TODO' 
+@app.route('/john')
+def john():
+    return 'PARSE TEXT'
+@app.route('/playlist', methods=['POST'])
+def playlist(tracks=None):
+    return render_template('spotify.html', tracks=request.form['tracks'])
 
 if __name__ == '__main__':
     app.run()
