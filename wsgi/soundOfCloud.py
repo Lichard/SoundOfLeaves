d6854852628924b2c723593647f3fbe06706d401
Raymond Tang <Raytray25@gmail.com>
2013-01-19 00:17:17 -0500

    perhaps this typo fix solves the templating issue

--- wsgi/soundOfCloud.py
+++ wsgi/soundOfCloud.py
@@ -7,7 +7,7 @@ def index():
 
 @app.route('/hello/')
 @app.route('/hello/<name>')
-def hello():
+def hello(name=None):
     return render_template('hello.html', name=name)
 
 if __name__ == '__main__':
