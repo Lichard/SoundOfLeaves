be7fa5fd2345c54d6e8e5f6cb889fd75231a1f1f
Raymond Tang <Raytray25@gmail.com>
2013-01-19 00:10:43 -0500

    templating test2

--- wsgi/soundOfCloud.py
+++ wsgi/soundOfCloud.py
@@ -1,13 +1,14 @@
-from flask import Flask
+from flask import Flask, reunder_template
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return 'Index'
 
-@app.route('/hello')
+@app.route('/hello/')
+@app.route('/hello/<name>')
 def hello():
-    return 'Hello'
+    return render_template('hello.html', name=name)
 
 if __name__ == '__main__':
     app.run()
