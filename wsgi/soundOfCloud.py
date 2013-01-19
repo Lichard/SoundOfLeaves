36fe876483819aef13bccf429c8c56eedcb916d4
Raymond Tang <Raytray25@gmail.com>
2013-01-18 23:58:28 -0500

    test routing

--- wsgi/soundOfCloud.py
+++ wsgi/soundOfCloud.py
@@ -1,14 +1,13 @@
 from flask import render_template
 app = Flask(__name__)
 
-@app.route("/hello/")
-@app.route('/hello/<name>')
-def hello():
-    return render_template('hello.html', name=name)
-
 @app.route("/")
-def hi():
-    return "hi"
+def index():
+    return 'Index'
+
+@app.route('/hello')
+def hello():
+    return 'Hello'
 
 if __name__ == '__main__':
     app.run()
