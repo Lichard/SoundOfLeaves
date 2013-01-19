93f440f4c4264cc8ce3b795e428daa6de649d3fd
Richard <ryli721@gmail.com>
2013-01-19 14:36:08 -0500

    merging from connie

--- wsgi/soundOfLeaves.py
+++ wsgi/soundOfLeaves.py
@@ -1,4 +1,8 @@
+import sys
 from flask import Flask, render_template, request
+from pyechonest import config
+from pyechonest import song
+config.ECHO_NEST_API_KEY="IY520EPU4LRFUIY8R"
 app = Flask(__name__)
 
 @app.route("/")
@@ -11,7 +15,14 @@ def ray():
 
 @app.route('/connie', methods=['POST','GET'])
 def connie():
-   return 'TODO get tracks, send to richard' 
+    songlist = song.search(mood=search, buckets=['id:spotify-WW', 'tracks'], limit=True, results=5)
+    foreign_ids = []
+    for item in songlist:
+        for t in item.get_tracks('spotify-WW'):
+            foreign_ids.append(t['foreign_id'][17:])
+        comma = ","
+        foreign_id_string = comma.join(foreign_ids)
+    return flask.render_template("spotify.html", tracks=foreign_id_string)
 
 @app.route('/john')
 def john():
